f8908c1aa887cce96774f575a641d82d8c902098
AlbertoAmayaSoria <albertoamayasoria@gmail.com>
2026-03-23 13:03:14 -0700

    avance

--- ultimate_tiktaktoe.py
+++ ultimate_tiktaktoe.py
@@ -1,181 +1,79 @@
 import juegos_simplificado as js
 
 class tiktaktoe(js.JuegoZT2):
-    """El juego ultimate tik tak toe"""
-    def init():
-        t = tuple(80 * [0])
-        t = t[:80] + (-2, -2)
-        return t
+    """Juego ultimate tik tak toe"""
+    def init(self):
+        return tuple([0] * 81 + [-2])
     
+    BLOQUES = {
+        (i, j): [(i*3 + r)*9 + (j*3 + c)
+                 for r in range(3)
+                 for c in range(3)]
+        for i in range(3)
+        for j in range(3)
+    }
+
     def jugadas_legales(self, s, j):
-        I_Arriba = {0, 1, 2, 
-                    9, 10, 11,
-                    18, 19, 20}
-        I_Centro = {27, 28, 29, 
-                    36, 37, 38, 
-                    45, 46, 47}
-        I_Abajo = {54, 55, 56,
-                   63, 64, 65,
-                   72, 73, 74}
-        C_Arriba = {3, 4, 5,
-                    12, 13, 14,
-                    21, 22, 23}
-        C_Centro = {30, 31, 32,
-                    39, 40,41,
-                    57, 58, 59}
-        C_Abajo = {57, 58, 59,
-                   66, 67, 68,
-                   75, 76, 77}
-        D_Arriba = {6, 7, 8,
-                    15, 16, 17,
-                    24, 25, 26}
-        D_Centro = {33, 34, 35,
-                    42, 43, 44,
-                    51, 52, 53}
-        D_Abajo = {60, 61, 62,
-                   69, 70, 71,
-                   78, 79, 80}
-        #Jugador 1
-        #No es eficiente
+        ultima = s[81]
 
-        if j > 0:
-            if s[81] in I_Arriba:
-                libres = [i for i in I_Arriba if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-                
-            elif s[81] in I_Centro:
-                libres = [i for i in I_Centro if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-            
-            elif s[81] in I_Abajo:
-                libres = [i for i in I_Abajo if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-            
-            elif s[81] in C_Arriba:
-                libres = [i for i in C_Arriba if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
+        #Primera jugada
+        if ultima == -2:
+            return [i for i in range(81) if s[i] == 0]
+        
+        fila = ultima // 9
+        col = ultima % 9
 
-            elif s[81] in C_Centro:
-                libres = [i for i in C_Centro if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
+        bf = fila % 3
+        bc = col % 3
 
-            elif s[81] in C_Abajo:
-                libres = [i for i in C_Abajo if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
+        bloque = self.BLOQUES[(bf, bc)]
+        libres = [i for i in bloque if s[i] == 0]
 
-            elif s[81] in D_Arriba:
-                libres = [i for i in D_Arriba if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
+        # si el bloque tiene espacio
+        if libres:
+            return libres
+        
+        # si el bloque est lleno
+        return [i for i in range(81) if s[i] == 0]
+        
+    def gano_bloque(self, tablero, bf, bc, j):
 
-            elif s[81] in D_Centro:
-                libres = [i for i in D_Centro if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
+        bloque = self.BLOQUES[(bf,bc)]
 
-            else: 
-                s[81] in D_Abajo
-                libres = [i for i in D_Abajo if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
+        c = [tablero[i] for i in bloque]
 
-        #Jugador 2
-        else:
+        lineas = [
+            # Izquierda-Derecha
+            (0,1,2),(3,4,5),(6,7,8),
+            # Arriba-Abajo
+            (0,3,6),(1,4,7),(2,5,8),
+            # Diagonales
+            (0,4,8),(2,4,6)
+        ]
 
-            if s[82] in I_Arriba:
-                libres = [i for i in I_Arriba if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-                
-            elif s[82] in I_Centro:
-                libres = [i for i in I_Centro if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-            
-            elif s[82] in I_Abajo:
-                libres = [i for i in I_Abajo if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-            
-            elif s[82] in C_Arriba:
-                libres = [i for i in C_Arriba if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
+        for a,b,c_ in lineas:
+            if c[a] == c[b] == c[c_] == j:
+                return True
 
-            elif s[82] in C_Centro:
-                libres = [i for i in C_Centro if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
+        return False
 
-            elif s[82] in C_Abajo:
-                libres = [i for i in C_Abajo if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-
-            elif s[82] in D_Arriba:
-                libres = [i for i in D_Arriba if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-
-            elif s[82] in D_Centro:
-                libres = [i for i in D_Centro if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-
-            else: 
-                s[82] in D_Abajo
-                libres = [i for i in D_Abajo if s[i] == 0]
-                if len(libres):
-                    return libres
-                else:
-                    return [posicion for posicion in range(81) if s[posicion] == 0]
-            
     def sucesor(self, s, a, j):
         if s[a] != 0:
             raise ValueError("Celda ocupada")
         
-        tablero = s[:81]
-        nuevo_tablero = tablero[:a] + (j,) + tablero[a+1:]
+        tablero = list(s[:81])
+        tablero[a] = j
+
+        fila = a // 9
+        col = a % 9
+
+        bf = fila // 3
+        bc = col // 3
+
+        if self.gano_bloque(tablero, bf, bc, j):
+            # Marcar como bloque ganado
+
+        tablero = tuple(tablero)
 
         if j == 1:
             jugada1 = a
@@ -184,4 +82,10 @@ class tiktaktoe(js.JuegoZT2):
             jugada1 = s[81]
             jugada2 = a
 
-        return nuevo_tablero + (jugada1, jugada2)
+        return tablero + (jugada1, jugada2)
+
+
+    def terminal(self, s):
+        #Revisar dónde se debe actualizar un "gato" pequeño para cambiarlo por completo a un valor
+        #Revisar si hay 3 bloques con el mismo número consecutivos
+        
